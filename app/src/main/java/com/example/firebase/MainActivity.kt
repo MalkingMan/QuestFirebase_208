5d4f2b728780b1954c54bda11a52e68e0d2bc234
MalkingMan <arrayalkhozini@gmail.com>
2026-01-03 16:56:59 +0700

    Add SiswaTopAppBar composable for navigation UI
    - Create `SiswaTopAppBar.kt` containing the `SiswaTopAppBar` composable function.
    - Implement `CenterAlignedTopAppBar` with title and optional back navigation logic.
    - Add support for scroll behavior and custom modifiers.

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -5,13 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.firebase.ui.theme.FirebaseTheme
+import com.example.firebase.view.controllNavigasi.PetaNavigasi
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +16,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FirebaseTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    PetaNavigasi()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirebaseTheme {
-        Greeting("Android")
     }
 }
